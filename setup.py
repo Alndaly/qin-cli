4d72f5a82445379356f3a0cb75986179821d1d26
Kinda Hall <1142704468@qq.com>
2024-06-01 22:55:31 +0800

    refactor: 💡 v0.0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("./LICENSE", "r", encoding="utf-8") as fp:
     
 setup(
     name="qin",
-    version="0.0.1",
+    version="0.0.2",
     keywords=["pip", "tools"],
     description="Interesting toolset",
     entry_points={
