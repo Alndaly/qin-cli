bdf394577cb8c1b6c26f1afd2dca74c12e2df144
Kinda Hall <1142704468@qq.com>
2024-06-01 21:55:57 +0800

    feat: 🎸 Add support to cut medias

--- qin/media.py
+++ qin/media.py
@@ -5,8 +5,54 @@ import time
 import yt_dlp
 from rich import print
 from typing import Annotated
+from pathlib import Path
+from pydub import AudioSegment
+from moviepy.editor import VideoFileClip, AudioFileClip, CompositeVideoClip
 
 media_app = typer.Typer()
+
+def is_video_file(filepath):
+    try:
+        # 尝试加载文件作为视频文件
+        video = VideoFileClip(filepath)
+        video.close()
+        return True
+    except Exception as e:
+        return False
+
+def is_audio_file(filepath):
+    try:
+        # 尝试加载文件作为音频文件
+        audio = AudioSegment.from_file(filepath)
+        return True
+    except Exception as e:
+        return False
+
+@media_app.command("2gif")
+def to_gif(input_file: Annotated[str, typer.Option("--input-file", "-i")], output_file: Annotated[str, typer.Option("--output-file", "-o")]):
+    video = VideoFileClip(input_file)
+    video.write_gif(output_file)
+    
+@media_app.command("cut")
+def cut(input_file: Annotated[str, typer.Option("--input-file", "-i")], start_time: Annotated[str, typer.Option("--start-time", "-s")], end_time: Annotated[str, typer.Option("--end-time", "-e")], output_file: Annotated[str, typer.Option("--output-file", "-o")] = None):
+    file_item = Path(input_file)
+    if output_file is None:
+        output_file = f"{file_item.stem} cutted{file_item.suffix}"
+    if is_video_file(input_file):
+        # 视频文件
+        video = VideoFileClip(input_file)
+        # 剪切视频
+        video_cut = video.subclip(start_time, end_time)
+        # 保存剪切后的视频
+        video_cut.write_videofile(output_file, codec="libx264")
+    elif is_audio_file(input_file):
+        # 音频文件
+        audio = AudioFileClip(input_file)
+        # 剪切视频
+        audio_cut = audio.subclip(start_time, end_time)
+        # 保存剪切后的视频
+        audio_cut.write_audiofile(output_file)
+   
 
 @media_app.command("convert")
 def convert(input_file: Annotated[str, typer.Option("--input-file", "-i")], output_file: Annotated[str, typer.Option("--output-file", "-o")]):
